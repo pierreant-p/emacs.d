fe72fe6519dc687bc5ef819682eb5bed7e0257e8
Pierre-Antoine Passet <pa.passet@gmail.com>
2016-04-10 21:25:14 +0200

    Sets font

--- core/init-ui.el
+++ core/init-ui.el
@@ -1,6 +1,9 @@
 ;; UI config
 
 
+;;----------------------------------------------------------
+;; General
+;;----------------------------------------------------------
 ;; Disable blinking cursor
 (blink-cursor-mode -1)
 
@@ -16,8 +19,20 @@
 ;; Highlight current line
 (global-hl-line-mode 1)
 
+;;----------------------------------------------------------
+;; Layout
+;;----------------------------------------------------------
 ;; Disable column split
 (setq split-height-threshold nil split-width-threshold nil)
+
+
+;;----------------------------------------------------------
+;; Font
+;;----------------------------------------------------------
+(when (window-system)
+  (set-frame-font "Source Code Pro")
+  (set-face-attribute 'default nil :font "Source Code Pro" :height 120)
+  (set-face-font 'default "Source Code Pro"))
 
 
 (provide 'init-ui)
