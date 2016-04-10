f9b33cf906c82f43890efebbc084097266a1e15e
Pierre-Antoine Passet <pa.passet@gmail.com>
2016-04-10 20:46:59 +0200

    Updates core ui

--- core/init-ui.el
+++ core/init-ui.el
@@ -8,11 +8,16 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
+(setq inhibit-startup-message t)
 
 ;; Show line number
 (global-linum-mode 1)
 
 ;; Highlight current line
 (global-hl-line-mode 1)
+
+;; Disable column split
+(setq split-height-threshold nil split-width-threshold nil)
+
 
 (provide 'init-ui)
