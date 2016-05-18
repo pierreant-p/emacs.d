a3bc06a294aa98569cfe06f536dcfef45aa97b8d
Pierre-Antoine Passet <pa.passet@gmail.com>
2016-05-18 17:37:54 +0200

    Updates ansible vault path

--- modules/init-ansible.el
+++ modules/init-ansible.el
@@ -9,6 +9,7 @@
 (global-set-key (kbd "C-c b") 'ansible::decrypt-buffer)
 (global-set-key (kbd "C-c e") 'ansible::encrypt-buffer)
 (add-hook 'ansible-hook 'ansible::auto-decrypt-encrypt)
+(setq ansible::vault-password-file "/Users/pilou/.vault.txt")
 
 (provide 'init-ansible)
 ;;; init-ansible.el ends here
